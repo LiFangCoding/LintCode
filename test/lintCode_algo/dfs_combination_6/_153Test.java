6e98be4dcb0616c957376cd7f37288578474bcc9
LiFangCoding <fanglihust@gmail.com>
2018-12-12 23:11:23 +0900

    _153

---
+++ test/lintCode_algo/dfs_combination_6/_153Test.java
@@ -0,0 +1,16 @@
+package lintCode_algo.dfs_combination_6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class _153Test {
+    _153 sol = new _153();
+
+    @Test
+    void combinationSum2() {
+        int[] input = {10, 1, 6, 7, 2, 1, 5};
+        String expected = "[[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]";
+        assertEquals(expected, sol.combinationSum2(input, 8).toString());
+    }
+}
