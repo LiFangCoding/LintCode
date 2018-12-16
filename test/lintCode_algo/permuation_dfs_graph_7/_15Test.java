ebe966f3b4645a224d47a2c4cd58264ef1fbeea5
LiFangCoding <fanglihust@gmail.com>
2018-12-16 18:40:03 +0900

    _15

---
+++ test/lintCode_algo/permuation_dfs_graph_7/_15Test.java
@@ -0,0 +1,13 @@
+package lintCode_algo.permuation_dfs_graph_7;
+
+import org.junit.jupiter.api.Test;
+
+class _15Test {
+    _15 sol = new _15();
+
+    @Test
+    void permute() {
+        int[] num = {1, 2, 3};
+        System.out.println(sol.permute(num));
+    }
+}
