1d310eb4e0b688f2b477f780cbe137841d11b0a1
LiFangCoding <fanglihust@gmail.com>
2018-12-17 21:45:06 +0900

    _33

---
+++ test/lintCode_algo/permuation_dfs_graph_7/_33Test.java
@@ -0,0 +1,12 @@
+package lintCode_algo.permuation_dfs_graph_7;
+
+import org.junit.jupiter.api.Test;
+
+class _33Test {
+    _33 sol = new _33();
+
+    @Test
+    void solveNQueens() {
+        System.out.println(sol.solveNQueens(4));
+    }
+}
