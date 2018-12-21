4d2bda7b736f0779aac9eec428116bbe869b18f0
LiFangCoding <fanglihust@gmail.com>
2018-12-21 21:27:06 +0900

    _132

--- src/lintCode_algo/dfs_perm_7/_829.java
+++ src/lintCode_algo/dfs_perm_7/_829.java
@@ -6,7 +6,17 @@ import java.util.Map;
 import java.util.Set;
 
 /**
+ *Given a pattern and a string str, find if str follows the same pattern.
  *
+ * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.(i.e if a corresponds to s, then b cannot correspond to s. For example, given pattern = "ab", str = "ss", return false.)
+ *
+ * Example
+ * Given pattern = "abab", str = "redblueredblue", return true.
+ * Given pattern = "aaaa", str = "asdasdasdasd", return true.
+ * Given pattern = "aabb", str = "xyzabcxzyabc", return false.
+ *
+ * Notice
+ * You may assume both pattern and str contains only lowercase letters.
  */
 public class _829 {
     public boolean wordPatternMatch(String s, String t) {
