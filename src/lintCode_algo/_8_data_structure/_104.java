3448b5e18d9e5970e2193a47d22dde5b50e9fb03
LiFangCoding <fanglihust@gmail.com>
2019-01-04 07:45:21 +0900

    _40

---
+++ src/lintCode_algo/_8_data_structure/_104.java
@@ -0,0 +1,24 @@
+package lintCode_algo._8_data_structure;
+
+/**
+ * Merge k sorted linked lists and return it as one sorted list.
+ * <p>
+ * Analyze and describe its complexity.
+ * <p>
+ * Have you met this question in a real interview?
+ * Example
+ * Given lists:
+ * <p>
+ * [
+ * 2->4->null,
+ * null,
+ * -1->null
+ * ],
+ * return -1->2->4->null.
+ */
+public class _104 {
+    //TODO
+    public static class sol1_DivideConquer {
+
+    }
+}
---
+++ src/lintCode_algo/_8_data_structure/_40.java
@@ -0,0 +1,61 @@
+package lintCode_algo._8_data_structure;
+
+import java.util.Stack;
+
+/**
+ * As the title described, you should only use two stacks to implement a queue's actions.
+ * <p>
+ * The queue should support push(element), pop() and top() where pop is pop the first(a.k.a front) element in the queue.
+ * <p>
+ * Both pop and top methods should return the value of first element.
+ */
+public class _40 {
+    public class MyQueue {
+        // s2 for top and pop method.
+        // s1 for push method.
+        Stack<Integer> s2;
+        Stack<Integer> s1;
+
+        public MyQueue() {
+            s2 = new Stack<>();
+            s1 = new Stack<>();
+        }
+
+        /*
+         * @param element: An integer
+         * @return: nothing
+         */
+        public void push(int element) {
+            s1.add(element);
+        }
+
+        /*
+         * @return: An integer
+         */
+        public int pop() {
+            // write your code here
+            if (s2.isEmpty()) {
+                moveStack(s1, s2);
+            }
+            return s2.pop();
+        }
+
+        /*
+         * @return: An integer
+         */
+        public int top() {
+            // write your code here
+            if (s2.isEmpty()) {
+                moveStack(s1, s2);
+            }
+            return s2.peek();
+        }
+
+        // move all s1 to s2
+        private void moveStack(Stack<Integer> s1, Stack<Integer> s2) {
+            while (!s1.isEmpty()) {
+                s2.add(s1.pop());
+            }
+        }
+    }
+}
