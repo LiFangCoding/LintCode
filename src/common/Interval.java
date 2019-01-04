de539e199a318353cd938315c3090a80b111e015
LiFangCoding <fanglihust@gmail.com>
2019-01-05 02:27:47 +0900

    _839

--- src/common/Interval.java
+++ src/common/Interval.java
@@ -1,14 +1,21 @@
-package lintCode_algo._9_data_structure_advanced;
+package common;
 
 /**
  * Merge two sorted (ascending) lists of interval and return it as a new sorted list. The new sorted list should be made by splicing together the intervals of the two lists and sorted in ascending order.
  * <p>
- * The intervals in the given list do not overlap.
- * The intervals in different lists may overlap.
- * Have you met this question in a real interview?
  * Example
  * Given list1 = [(1,2),(3,4)] and list2 = [(2,3),(5,6)], return [(1,4),(5,6)].
+ * <p>
+ * Notice
+ * The intervals in the given list do not overlap.
+ * The intervals in different lists may overlap.
  */
-public class _839_Continue {
+public class Interval {
+    public int start;
+    public int end;
 
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
 }
---
+++ test/lintCode_algo/_9_data_structure_advanced/_839_Test.java
@@ -0,0 +1,24 @@
+package lintCode_algo._9_data_structure_advanced;
+
+import common.Interval;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class _839_Test {
+    _839 sol = new _839();
+
+    @Test
+    void mergeTwoInterval() {
+        List<Interval> list1 = new ArrayList<>();
+        list1.add(new Interval(1, 2));
+        list1.add(new Interval(3, 4));
+
+        List<Interval> list2 = new ArrayList<>();
+        list2.add(new Interval(2, 3));
+        list2.add(new Interval(5, 6));
+
+        System.out.println(sol.mergeTwoInterval(list1, list2));
+    }
+}
