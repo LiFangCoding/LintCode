1f5e80eef2cf96926ef610fe43f0646d22788ce9
LiFangCoding <fanglihust@gmail.com>
2019-01-09 20:29:09 +0900

    _547_sol3_sortAndBinarySearch

--- src/lintCode_alg/_9_data_structure_advanced/_547.java
+++ src/lintCode_alg/_9_data_structure_advanced/_547.java
@@ -17,6 +17,7 @@ import java.util.*;
 public class _547 {
     public static class Sol1_sortAndMerge {
         //T: O(nlogn) + O(mlogm) + O(min(m, n))
+        // S: O(size of intersection)
         public int[] intersection(int[] left, int[] right) {
             // write your code here
             if (left == null || right == null) {
@@ -56,6 +57,8 @@ public class _547 {
         }
     }
 
+    //T: O(max{left, right})
+    //S: O(size of interception)
     public static class Sol2_hashMap {
         public int[] intersection(int[] left, int[] right) {
             if (left == null || right == null) {
@@ -79,4 +82,61 @@ public class _547 {
             return res.stream().mapToInt(x -> x).toArray();
         }
     }
+
+    //T: O((n + m) * logn), n choose the small size.
+    public static class sol3_binary_search {
+        public int[] intersection(int[] left, int[] right) {
+            if (left == null || right == null) {
+                return null;
+            }
+
+            if (left.length > right.length) {
+                int[] temp = left;
+                left = right;
+                right = temp;
+            }
+
+            Set<Integer> set = new HashSet<>();
+
+            Arrays.sort(left);
+            for (int i : right) {
+                if (find(i, left)) {
+                    set.add(i);
+                }
+            }
+
+            int[] res = new int[set.size()];
+            int index = 0;
+            for (int i : set) {
+                res[index++] = i;
+            }
+
+            return res;
+        }
+
+        private boolean find(int target, int[] A) {
+            //Imporatnt: Consider corner ase if A is empty.
+            if (A == null || A.length == 0) {
+                return false;
+            }
+
+            int start = 0;
+            int end = A.length - 1;
+
+            while (start + 1 < end) {
+                int mid = start + (end - start) / 2;
+                if (A[mid] == target) {
+                    return true;
+                } else if (A[mid] < target) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+
+            return A[start] == target || A[end] == target;
+        }
+    }
+
+
 }
