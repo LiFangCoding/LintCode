6d53dc3ede1135e5f33c7bd7124959e696d09930
LiFangCoding <fanglihust@gmail.com>
2019-04-28 16:23:32 +0900

    EPI_4_07 doing

---
+++ src/epi_book/_4_primitive_types/_7_ComputeXPowerY.java
@@ -0,0 +1,7 @@
+package epi_book._4_primitive_types;
+
+public class _7_ComputeXPowerY {
+    public static double power(double x, double y) {
+        return 1;
+    }
+}
---
+++ test/epi_book/_4_primitive_types/_7_ComputeXPowerYTest.java
@@ -0,0 +1,11 @@
+package epi_book._4_primitive_types;
+
+import org.junit.jupiter.api.Test;
+
+class _7_ComputeXPowerYTest {
+
+    @Test
+    void power() {
+
+    }
+}
